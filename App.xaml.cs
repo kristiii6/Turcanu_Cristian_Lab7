2f8bfffaded23d3ff09ea5c51321036eeec5a3ec
CRISTIAN ȚURCANU <cristian.turcanu1@stud.ubbcluj.ro>
2023-12-19 02:27:26 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Turcanu_Cristian_Lab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Turcanu_Cristian_Lab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
